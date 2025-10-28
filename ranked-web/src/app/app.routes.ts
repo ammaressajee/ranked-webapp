82f572c534020aab739cc942e096bb2789db8670
Ammar Essajee <ammar.essajee97@gmail.com>
2025-10-28 14:45:47 -0500

    added fix for mvp

--- ranked-web/src/app/app.routes.ts
+++ ranked-web/src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { RecordMatchComponent } from './pages/record-match/record-match.componen
 import { PlayerProfileComponent } from './components/player-profile/player-profile.component';
 
 export const routes: Routes = [
-
+    { path: '', component: LeaderboardComponent },
     // Define a path for your login screen
     { path: 'login', component: LoginComponent },
     { path: 'leaderboard', component: LeaderboardComponent },
     { path: 'record-match', component: RecordMatchComponent },
     { path: 'profile/:uid', component: PlayerProfileComponent },
+
 
     // Catch-all or redirect
     { path: '**', redirectTo: '' }
